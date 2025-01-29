e35c75d2d10db7e89dad4759373912f7f054d286
Gas <gas.gasan2000@yandex.ru>
2025-01-29 17:38:07 +0500

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,12 +1,19 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import getParse from '../src/index.js';
 
 program
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('13.0.0')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
     .helpOption('-V, --version', 'output the version number')
-    .helpOption('-h, --help', 'output usage information');
+    .option('-f, --format [type]', 'output format')
+    .helpOption('-h, --help', 'output usage information')
+    .action((filepath1, filepath2) => {
+        console.log(getParse(filepath1, filepath2))
+    });
 
 program.parse();
