301a90040a077d80c9435b053351f57ef5918564
Gas <gas.gasan2000@yandex.ru>
2025-02-02 18:29:35 +0500

    add index.test.js

---
+++ __test__/index.test.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import path from 'path';
+import { dirname } from 'path';
+import genDiff from "../src/index.js";
+import { expect, test } from '@jest/globals';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+
+const getFullPath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
+const readFile = (filename) => fs.readFileSync(getFullPath(filename), 'utf-8');
+
+test('gendiff', () => {
+    expect(genDiff(
+      getFullPath('file1.json'),
+      getFullPath('file2.json')
+    )).toEqual(readFile('result.txt'));
+})
