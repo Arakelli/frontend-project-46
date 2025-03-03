3a7effd0c1b198363f6c88753fcbcb428e9c12ba
Gas <gas.gasan2000@yandex.ru>
2025-03-03 12:50:19 +0500

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,41 +1,26 @@
 import path from 'path';
-import { dirname } from 'path';
+import fs from 'fs';
 import genDiff from "../src/index.js";
 import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 
-const getFullPath = (filepath) => path.join(__dirname, '..', '__fixtures__', filepath);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff-json', () => {
-    const result = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}`
     expect(genDiff(
-      getFullPath('file1.json'),
-      getFullPath('file2.json')
-    )).toBe(result);
+      getFixturePath('file1.json'),
+      getFixturePath('file2.json')
+    )).toEqual(readFile('result.txt').trim());
 });
 
 test('gendiff-yaml', () => {
-  const result = `{
- - follow: false
-   host: hexlet.io
- - proxy: 123.234.53.22
- - timeout: 50
- + timeout: 20
- + verbose: true
-}`
   expect(genDiff(
-    getFullPath('file1.yaml'),
-    getFullPath('file2.yaml')
-  )).toBe(result);
+    getFixturePath('file1.yaml'),
+    getFixturePath('file2.yaml')
+  )).toEqual(readFile('result.txt').trim());
 });
