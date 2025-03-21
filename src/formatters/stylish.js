c70cdc23165d9ac19ae5e7c7efa8fcb96956bbe5
Gas <gas.gasan2000@yandex.ru>
2025-03-21 19:01:03 +0500

    update stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,31 +1,54 @@
-import stringify from "../stringify.js";
+import _ from 'lodash';
 
-const stylish = (tree, depth = 1) => {
-    const spacesCount = 4;
-    const shiftLet = 2;
-    const indent = ' '.repeat(depth * spacesCount - shiftLet);
+const replacer = '    ';
 
-    const result = tree
-        .map((node) => {
-            switch (node.type) {
-                case 'added':
-                    return `${indent}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
-                case 'removed':
-                    return `${indent}- ${node.key}: ${stringify(node.value, depth + 1)}`;
-                case 'unchanged':
-                    return `${indent}  ${node.key}: ${stringify(node.value, depth + 1)}`;
-                case 'changed':
-                    return `${indent}- ${node.key}: ${stringify(node.oldValue, depth + 1)}\n${node.key}: ${stringify(node.newValue, depth + 1)}`;
-                case 'nested':
-                    return `${indent}  ${node.key}: ${stringify(node.value, depth + 1)}`;
-                default:
-                    throw new Error(`Unknow type: ${node.type}!`);  
-            }
-        })
-        .join('\n');
+const stringify = (data, depth) => {
+  if (!_.isObject(data)) {
+    return `${data}`;
+  }
 
-    const outerIndent = ' '.repeat((depth - 1) * spacesCount);
-    return `{\n${result}\n${outerIndent}}`;
+  const currentReplacer = replacer.repeat(depth);
+  const entries = Object.entries(data);
+  const strings = entries.map(
+    ([key, value]) => `${currentReplacer}    ${key}: ${stringify(value, depth + 1)}`,
+  );
+  return `{\n${strings.join('\n')}\n${currentReplacer}}`;
+};
+
+const stylish = (data) => {
+    const replacer = '    ';
+
+    const iter = (obj, depth) => {
+      const currentReplacer = replacer.repeat(depth);
+      const result = obj.flatMap((node) => {
+        const {
+          key, oldValue, newValue, value, type, children,
+        } = node;
+        switch (type) {
+          case 'added':
+            return `${currentReplacer}  + ${key}: ${stringify(value, depth + 1)}`;
+          case 'deleted':
+            return `${currentReplacer}  - ${key}: ${stringify(value, depth + 1)}`;
+          case 'unchanged':
+            return `${currentReplacer}    ${key}: ${stringify(value, depth + 1)}`;
+          case 'changed':
+            return `${currentReplacer}  - ${key}: ${stringify(
+              oldValue,
+              depth + 1,
+            )}\n${currentReplacer}  + ${key}: ${stringify(newValue, depth + 1)}`;
+          case 'hasChild':
+            return `${currentReplacer}    ${key}: ${iter(children, depth + 1)}`;
+          default:
+            throw new Error(
+              `Unexpected type encountered: ${type}. Node details: ${JSON.stringify(
+                node,
+              )}`,
+            );
+        }
+      });
+      return `{\n${result.join('\n')}\n${currentReplacer}}`;
+    };
+    return iter(data, 0);
 };
 
 export default stylish;
