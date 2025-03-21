464627d0c49bbef23684c02329cd6dec5dd254a6
Gas <gas.gasan2000@yandex.ru>
2025-03-21 19:00:41 +0500

    update index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,7 +1,20 @@
+import json from "./json.js";
+import plain from "./plain.js";
 import stylish from "./stylish.js";
 
-const formatter = (tree) => {
-    return stylish(tree)
+const formatter = (tree, format) => {
+    switch (format) {
+        case 'stylish':
+            return stylish(tree);
+        case 'plain':
+            return plain(tree);
+        case 'json':
+            return json(tree);
+        default:
+            throw new Error(
+                `Format ${format} not supported. Choose 'stylish', 'plain' or 'json'`
+            );
+    }
 };
 
 export default formatter;
