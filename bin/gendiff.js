1395015b8419c1caf4f38baf9abd8239c0f2411a
Gas <gas.gasan2000@yandex.ru>
2025-03-21 19:03:23 +0500

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -10,10 +10,10 @@ program
     .argument('<filepath1>')
     .argument('<filepath2>')
     .helpOption('-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format')
+    .option('-f, --format [type]', 'output format', 'stylish')
     .helpOption('-h, --help', 'output usage information')
-    .action((filepath1, filepath2) => {
-        console.log(genDiff(filepath1, filepath2));
+    .action((filepath1, filepath2, options) => {
+        console.log(genDiff(filepath1, filepath2, options.format));
     });
 
 program.parse();
