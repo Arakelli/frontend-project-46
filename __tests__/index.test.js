57620c8436127ed2e038a29df94d1cc6b46015bf
Gas <gas.gasan2000@yandex.ru>
2025-03-24 10:41:51 +0500

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,26 @@
-import path from 'path';
 import fs from 'fs';
-import genDiff from "../src/index.js";
+import path from 'path';
 import { expect, test } from '@jest/globals';
 import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('gendiff-json', () => {
-    expect(genDiff(
-      getFixturePath('file1.json'),
-      getFixturePath('file2.json')
-    )).toEqual(readFile('result-stylish.txt').trim());
+  expect(genDiff(
+    getFixturePath('file1.json'),
+    getFixturePath('file2.json'),
+  )).toEqual(readFile('result-stylish.txt').trim());
 });
 
 test('gendiff-yaml', () => {
   expect(genDiff(
     getFixturePath('file1.yaml'),
-    getFixturePath('file2.yaml')
+    getFixturePath('file2.yaml'),
   )).toEqual(readFile('result-stylish.txt').trim());
 });
 
@@ -37,6 +36,6 @@ test('gendiff-json-json_formant', () => {
   expect(genDiff(
     getFixturePath('file1.json'),
     getFixturePath('file2.json'),
-    'json'
+    'json',
   )).toEqual(readFile('result-json.txt').trim());
 });
