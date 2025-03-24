a479a4c8a11a6bb2fa539be3720f7861ba0f957b
Gas <gas.gasan2000@yandex.ru>
2025-03-24 10:42:26 +0500

    fix gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,16 +4,16 @@ import { program } from 'commander';
 import genDiff from '../src/index.js';
 
 program
-    .name('gendiff')
-    .description('Compares two configuration files and shows a difference.')
-    .version('13.0.0')
-    .argument('<filepath1>')
-    .argument('<filepath2>')
-    .helpOption('-V, --version', 'output the version number')
-    .option('-f, --format [type]', 'output format', 'stylish')
-    .helpOption('-h, --help', 'output usage information')
-    .action((filepath1, filepath2, options) => {
-        console.log(genDiff(filepath1, filepath2, options.format));
-    });
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('13.0.0')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
+  .helpOption('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2, options) => {
+    console.log(genDiff(filepath1, filepath2, options.format));
+  });
 
 program.parse();
