e2eb0be0531476daa8425248f498668f9f40d8ab
Gas <gas.gasan2000@yandex.ru>
2025-03-24 10:42:51 +0500

    fix parser.js

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,11 @@
 import yaml from 'js-yaml';
 
 export default (data, format) => {
-    if (format === '.json') {
-        return JSON.parse(data);
-    }
-    if (format === '.yml' || format ==='.yaml') {
-        return yaml.load(data);
-    }
-    throw Error('Unknow format');
+  if (format === '.json') {
+    return JSON.parse(data);
+  }
+  if (format === '.yml' || format === '.yaml') {
+    return yaml.load(data);
+  }
+  throw Error('Unknow format');
 };
