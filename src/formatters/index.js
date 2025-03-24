ff85c70f4e214f9d25bccc5f3733c7d654f22d6f
Gas <gas.gasan2000@yandex.ru>
2025-03-24 10:43:50 +0500

    fix index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,20 +1,20 @@
-import json from "./json.js";
-import plain from "./plain.js";
-import stylish from "./stylish.js";
+import json from './json.js';
+import plain from './plain.js';
+import stylish from './stylish.js';
 
 const formatter = (tree, format) => {
-    switch (format) {
-        case 'stylish':
-            return stylish(tree);
-        case 'plain':
-            return plain(tree);
-        case 'json':
-            return json(tree);
-        default:
-            throw new Error(
-                `Format ${format} not supported. Choose 'stylish', 'plain' or 'json'`
-            );
-    }
+  switch (format) {
+    case 'stylish':
+      return stylish(tree);
+    case 'plain':
+      return plain(tree);
+    case 'json':
+      return json(tree);
+    default:
+      throw new Error(
+        `Format ${format} not supported. Choose 'stylish', 'plain' or 'json'`,
+      );
+  }
 };
 
 export default formatter;
