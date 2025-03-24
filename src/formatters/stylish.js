c0a380ff307d7e3294e37953120cff4a4f7fd1d6
Gas <gas.gasan2000@yandex.ru>
2025-03-24 10:44:27 +0500

    fix stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -16,39 +16,39 @@ const stringify = (data, depth) => {
 };
 
 const stylish = (data) => {
-    const replacer = '    ';
+  const replacer = '    ';
 
-    const iter = (obj, depth) => {
-      const currentReplacer = replacer.repeat(depth);
-      const result = obj.flatMap((node) => {
-        const {
-          key, oldValue, newValue, value, type, children,
-        } = node;
-        switch (type) {
-          case 'added':
-            return `${currentReplacer}  + ${key}: ${stringify(value, depth + 1)}`;
-          case 'deleted':
-            return `${currentReplacer}  - ${key}: ${stringify(value, depth + 1)}`;
-          case 'unchanged':
-            return `${currentReplacer}    ${key}: ${stringify(value, depth + 1)}`;
-          case 'changed':
-            return `${currentReplacer}  - ${key}: ${stringify(
-              oldValue,
-              depth + 1,
-            )}\n${currentReplacer}  + ${key}: ${stringify(newValue, depth + 1)}`;
-          case 'hasChild':
-            return `${currentReplacer}    ${key}: ${iter(children, depth + 1)}`;
-          default:
-            throw new Error(
-              `Unexpected type encountered: ${type}. Node details: ${JSON.stringify(
-                node,
-              )}`,
-            );
-        }
-      });
-      return `{\n${result.join('\n')}\n${currentReplacer}}`;
-    };
-    return iter(data, 0);
+  const iter = (obj, depth) => {
+    const currentReplacer = replacer.repeat(depth);
+    const result = obj.flatMap((node) => {
+      const {
+        key, oldValue, newValue, value, type, children,
+      } = node;
+      switch (type) {
+        case 'added':
+          return `${currentReplacer}  + ${key}: ${stringify(value, depth + 1)}`;
+        case 'deleted':
+          return `${currentReplacer}  - ${key}: ${stringify(value, depth + 1)}`;
+        case 'unchanged':
+          return `${currentReplacer}    ${key}: ${stringify(value, depth + 1)}`;
+        case 'changed':
+          return `${currentReplacer}  - ${key}: ${stringify(
+            oldValue,
+            depth + 1,
+          )}\n${currentReplacer}  + ${key}: ${stringify(newValue, depth + 1)}`;
+        case 'hasChild':
+          return `${currentReplacer}    ${key}: ${iter(children, depth + 1)}`;
+        default:
+          throw new Error(
+            `Unexpected type encountered: ${type}. Node details: ${JSON.stringify(
+              node,
+            )}`,
+          );
+      }
+    });
+    return `{\n${result.join('\n')}\n${currentReplacer}}`;
+  };
+  return iter(data, 0);
 };
 
 export default stylish;
