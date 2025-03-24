99d955869df9ef9ad2298caf4524c61a9b05c8fb
Gas <gas.gasan2000@yandex.ru>
2025-03-24 10:43:09 +0500

    fix index.js

--- src/index.js
+++ src/index.js
@@ -10,18 +10,18 @@ const getFullPath = (filepath) => path.resolve(process.cwd(), filepath);
 const getData = (filepath) => fs.readFileSync(getFullPath(filepath), 'utf-8');
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-    const dataFile1 = getData(filepath1);
-    const dataFile2 = getData(filepath2);
+  const dataFile1 = getData(filepath1);
+  const dataFile2 = getData(filepath2);
 
-    const formatFile1 = path.extname(filepath1);
-    const formatFile2 = path.extname(filepath2);
+  const formatFile1 = path.extname(filepath1);
+  const formatFile2 = path.extname(filepath2);
 
-    const parserData1 = parser(dataFile1, formatFile1);
-    const parserData2 = parser(dataFile2, formatFile2);
+  const parserData1 = parser(dataFile1, formatFile1);
+  const parserData2 = parser(dataFile2, formatFile2);
 
-    const buildTree = compareData(parserData1, parserData2);
+  const buildTree = compareData(parserData1, parserData2);
 
-    return `${getFormatter(buildTree, format)}`;
-}
+  return `${getFormatter(buildTree, format)}`;
+};
 
 export default genDiff;
